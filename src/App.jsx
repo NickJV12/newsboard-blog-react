60d147562a988d7c947a0848bb5a268d9e70fd9a
Shambhavi Mishra <shambhavim1202@gmail.com>
2026-07-19 16:22:44 +0530

    Install routing, axios and configure Tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-
 function App() { //react function component(parent)
   return (  //return JSX(Javascript XML)
-    <div>
-        <h1>NewsBoard</h1>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+        <h1 className="text-5xl font-bold text-blue-600">
+          NewsBoard
+        </h1>
     </div>
   );
 }
